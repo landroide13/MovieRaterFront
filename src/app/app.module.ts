44d72bccc0ae9f92671101b360b69b5eba92f955
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-09-09 12:21:05 +1200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailsComponent,
+    MovieFormComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
